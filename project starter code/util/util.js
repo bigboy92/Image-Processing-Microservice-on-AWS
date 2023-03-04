203b1b0e057f54bc893970d8d65ae59e7e8050ea
James Kekong <31131362+bigboy92@users.noreply.github.com>
2023-03-04 11:07:14 +0100

    added axios to process all image URLs

--- project starter code/util/util.js
+++ project starter code/util/util.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import Jimp from "jimp";
-
+import axios from 'axios';
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -12,8 +12,13 @@ import Jimp from "jimp";
  export async function filterImageFromURL(inputURL) {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
-      const outpath =
+      const { data: imageBuffer } = await axios({
+        method: 'get',
+        url: inputURL,
+        responseType: 'arraybuffer'
+      });
+      const photo = await Jimp.read(imageBuffer);
+        const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
         .resize(256, 256) // resize
